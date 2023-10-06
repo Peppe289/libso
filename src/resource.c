5ce0a3b4ac3dba8254d42a5f464c2df9bdd83b7e
NextWork123 <nextworks@protonmail.com>
2023-10-07 00:18:24 +0200

    src: Format the entire code and beautify

--- src/resource.c
+++ src/resource.c
@@ -1,16 +1,15 @@
 /*
- * resource.c
+ *resource.c
  *
- *  Created on: 11/apr/2013
- *      Author: user
+ *Created on: 11/apr/2013
+ *     Author: user
  */
 
 /*
- * RUSAGE_THREAD non restituisce i dati in modo completo o corretto.
- * Nel caso in cui si volesse usare printResourceUsage(), nel programma con i
- * thread usare RUSAGE_SELF
+ *RUSAGE_THREAD non restituisce i dati in modo completo o corretto.
+ *Nel caso in cui si volesse usare printResourceUsage(), nel programma con i
+ *thread usare RUSAGE_SELF
  */
-
 #include "ourhdr.h"
 
 struct timeb g_TStart, g_TStop;
@@ -21,8 +20,8 @@ static short countingStarted = FALSE;
 static short countingStopped = FALSE;
 
 /**
- * Replaces ftime() which is now deprecated.
- * The use is identical.
+ *Replaces ftime() which is now deprecated.
+ *The use is identical.
  */
 static void ftime_new(struct timeb *tb)
 {
@@ -59,7 +58,7 @@ void startCounting()
 		err_ret("Can't get starting values of resource usage");
 	if (getrusage(RUSAGE_CHILDREN, g_RChildrenStart) < 0)
 		err_ret("Can't get starting values of resource usage");
-	if (getrusage(RUSAGE_THREAD, g_RThreadStart) < 0) // @suppress("Symbol is not resolved")
+	if (getrusage(RUSAGE_THREAD, g_RThreadStart) < 0)	// @suppress("Symbol is not resolved")
 		err_ret("Can't get starting values of resource usage");
 }
 
@@ -86,7 +85,7 @@ void stopCounting()
 		err_ret("Can't get final values of resource usage");
 	if (getrusage(RUSAGE_CHILDREN, g_RChildrenStop) < 0)
 		err_ret("Can't get final values of resource usage");
-	if (getrusage(RUSAGE_THREAD, g_RThreadStop) < 0) // @suppress("Symbol is not resolved")
+	if (getrusage(RUSAGE_THREAD, g_RThreadStop) < 0)	// @suppress("Symbol is not resolved")
 		err_ret("Can't get final values of resource usage");
 }
 
@@ -96,7 +95,7 @@ static void printDataFromResource(struct rusage *start, struct rusage *stop)
 	print_err("Total Sys Time    : %.3f\n", TimeDiffuSec(start->ru_stime, stop->ru_stime));
 	print_err("Total User Time   : %.3f\n", TimeDiffuSec(start->ru_utime, stop->ru_utime));
 	print_err("Blocking I/O oper.: %ld (in) %ld (out)\n",
-			  stop->ru_inblock - start->ru_inblock, stop->ru_oublock - start->ru_oublock);
+		stop->ru_inblock - start->ru_inblock, stop->ru_oublock - start->ru_oublock);
 
 	print_err("Max resident size : %ld\n", stop->ru_maxrss);
 	print_err("Integral shared memory size: %ld\n", stop->ru_ixrss);
@@ -104,7 +103,7 @@ static void printDataFromResource(struct rusage *start, struct rusage *stop)
 	print_err("Integral unshared stack size: %ld\n", stop->ru_isrss);
 	print_err("Page reclaims (soft page faults): %ld\n", stop->ru_minflt);
 	print_err("Page faults (hard page faults): %ld\n", stop->ru_majflt);
-	print_err("# Swap operations: %ld\n\n", stop->ru_nswap); /* swaps */
+	print_err("# Swap operations: %ld\n\n", stop->ru_nswap); /*swaps */
 }
 
 void printResourceUsage(int who)
@@ -114,21 +113,21 @@ void printResourceUsage(int who)
 
 	switch (who)
 	{
-	case RUSAGE_SELF:
-		printDataFromResource(g_RStart, g_RStop);
-		break;
+		case RUSAGE_SELF:
+			printDataFromResource(g_RStart, g_RStop);
+			break;
 
-	case RUSAGE_CHILDREN:
-		printDataFromResource(g_RChildrenStart, g_RChildrenStop);
-		break;
+		case RUSAGE_CHILDREN:
+			printDataFromResource(g_RChildrenStart, g_RChildrenStop);
+			break;
 
-	case RUSAGE_THREAD: // @suppress("Symbol is not resolved")
-		printDataFromResource(g_RThreadStart, g_RThreadStop);
-		break;
+		case RUSAGE_THREAD:	// @suppress("Symbol is not resolved")
+			printDataFromResource(g_RThreadStart, g_RThreadStop);
+			break;
 
-	default:
-		print_err_func("no instructions found for %d value\n", who);
-		break;
+		default:
+			print_err_func("no instructions found for %d value\n", who);
+			break;
 	}
 }
 
@@ -157,6 +156,7 @@ double getUserTime()
 	else
 		return TimeDiffuSec(g_RStart->ru_utime, g_RStop->ru_utime);
 }
+
 double TimeDiffmSec(struct timeb Start, struct timeb Stop)
 {
 	double diff;
@@ -166,6 +166,7 @@ double TimeDiffmSec(struct timeb Start, struct timeb Stop)
 		Stop.millitm += 1000;
 		Stop.time -= 1;
 	}
+
 	diff = (double)(Stop.time - Start.time) + (Stop.millitm - Start.millitm) / 1000.;
 	return diff;
 }
@@ -177,28 +178,24 @@ double TimeDiffuSec(struct timeval Start, struct timeval Stop)
 		Stop.tv_usec += 1000000;
 		Stop.tv_sec -= 1;
 	}
+
 	return (double)(Stop.tv_sec - Start.tv_sec + (Stop.tv_usec - Start.tv_usec) / 1000000.);
 }
 
-/* make free all heap space used runtime */
+/*make free all heap space used runtime */
 void freeData()
 {
-	struct rusage **arr[] = {
-		&g_RChildrenStart,
-		&g_RChildrenStop,
-		&g_RStart,
-		&g_RStop,
-		&g_RThreadStart,
-		&g_RThreadStop,
+	struct rusage **arr[] = { &g_RChildrenStart, &g_RChildrenStop, &g_RStart, &g_RStop, &g_RThreadStart, &g_RThreadStop,
 	};
 
 	size_t arrSize = sizeof(arr) / sizeof(arr[0]);
 
-    for (size_t i = 0; i < arrSize; ++i) {
-		if (*arr[i] != NULL) {
-        free(*arr[i]);
-        *arr[i] = NULL;
-    }
-
-  }
+	for (size_t i = 0; i < arrSize; ++i)
+	{
+		if (*arr[i] != NULL)
+		{
+			free(*arr[i]);
+			*arr[i] = NULL;
+		}
+	}
 }
